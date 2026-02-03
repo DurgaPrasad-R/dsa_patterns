b8a7de741484cb3db18253c3a321d36c0e357203
Runkana Durga Prasad <runkanaprasad1@gmail.com>
2026-02-03 08:27:00 +0530

    Time: 19 ms (98.07%) | Memory: 43 MB (56.69%) - LeetSync

--- 79-word-search/word-search.java
+++ 79-word-search/word-search.java
@@ -2,28 +2,43 @@ class Solution {
     public boolean exist(char[][] board, String word) {
         int rows = board.length;
         int cols = board[0].length;
+        Map<Character, Integer> freqMap = new HashMap<>();
+        
+        for (int r = 0 ; r < rows; r++) {
+            for (int c = 0; c < cols; c++) {
+                freqMap.put(board[r][c], freqMap.getOrDefault(board[r][c], 0) + 1);
+            }
+        }
+
+        for (char ch : word.toCharArray()) {
+            if (!freqMap.containsKey(ch) || freqMap.get(ch) <= 0) {
+                return false;
+            }
+            freqMap.put(ch, freqMap.get(ch) - 1);
+        }
 
         for (int r = 0; r < rows; r++) {
             for (int c = 0; c < cols; c++) {
-                if (dfs(r, c, 0, board, word, new HashSet<>())) return true;
+                if (dfs(r, c, 0, board, word)) return true;
             }
         }
 
         return false;
     }
 
-    public boolean dfs(int r, int c, int idx, char[][] board, String word, Set<String> visited) {
+    public boolean dfs(int r, int c, int idx, char[][] board, String word) {
         if (idx == word.length()) {
             return true;
         }
 
-        if (r < 0 || r >= board.length || c < 0 || c >= board[0].length || visited.contains(r + "," + c) || board[r][c] != word.charAt(idx)) {
+        if (r < 0 || r >= board.length || c < 0 || c >= board[0].length || board[r][c] == '#' || board[r][c] != word.charAt(idx)) {
             return false;
         }
 
-        visited.add(r + "," + c);
-        boolean res = dfs(r - 1, c, idx + 1, board, word, visited) || dfs(r + 1, c, idx + 1, board, word,  visited) || dfs(r, c + 1, idx + 1, board, word, visited) || dfs(r, c - 1, idx + 1, board, word, visited);
-        visited.remove(r + "," + c);
+        char temp = board[r][c];
+        board[r][c] = '#';
+        boolean res = dfs(r - 1, c, idx + 1, board, word) || dfs(r + 1, c, idx + 1, board, word) || dfs(r, c + 1, idx + 1, board, word) || dfs(r, c - 1, idx + 1, board, word);
+        board[r][c] = temp;
         return res;
     }
 }
